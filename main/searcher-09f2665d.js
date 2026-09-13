ab33e7bef55bf9d74f2a11878680e21a34eeeb78
AzurIce <AzurIce@users.noreply.github.com>
2026-09-13 06:30:25 +0000

    deploy: AzurIce/ranim@79ffd05bdd504a7eb456bc4d8b24acf258023f28

--- main/searcher-09f2665d.js
+++ main/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-11ddeaed.js',
+                path_to_root + 'searchindex-87fd3d9b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
